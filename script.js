755197b29e959a3f8d264e9de902c759137a7d1f
raphatapi <raphaeltapioca10@gmail.com>
2022-01-05 20:16:03 -0700

    working on slide

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
-    var dateObj = Date.now();
-    dateObj += 1000 * 60 * 60 * 24 * 7;
-    // create a new Date object, using the adjusted time
-    dateObj = new Date(dateObj);
-    
     $('.autoplay').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 6000,
         arrows: false,
         centerMode: true,
-        centerPadding: '60px',
-        initialSlide: dateObj
+        centerPadding: '60px'
       });
+    
+    
+    var now = new Date();
+    var target = now.getDay();
+    console.log(target);
+    if (target == 2) {
+        console.log("yay!")
+        $('.autoplay').slick('slickCurrentSlide');
+   }      
   });
